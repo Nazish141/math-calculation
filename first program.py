547ee8c94c9f312f35fb756dee093898e499aab1
Nazish <nazishnoreen01@email.com>
2025-07-06 19:34:54 +0500

    my third push

--- first program.py
+++ first program.py
@@ -1,7 +1,8 @@
-str1 = "This is nazish\n she is good girl."
-print(str1)
-#contaminated version juda hoy words
-str2 = "nazish"
-str3 = "noor"
-print(str2 +str3)
-print("testing for git hub")
+# str1 = "This is nazish\n she is good girl."
+# print(str1)
+# #contaminated version juda hoy words
+# str2 = "nazish"
+# str3 = "noor"
+# print(str2 +str3)
+# print("testing for git hub")
+print("my name is nazish")
