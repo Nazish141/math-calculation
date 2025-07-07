9c3561e84291a1eccd0f08bd55da5493e362ac27
Nazish <nazishnoreen01@email.com>
2025-07-07 13:28:19 +0500

    my new file

--- first program.py
+++ first program.py
@@ -1,8 +1,2 @@
-# str1 = "This is nazish\n she is good girl."
-# print(str1)
-# #contaminated version juda hoy words
-# str2 = "nazish"
-# str3 = "noor"
-# print(str2 +str3)
-# print("testing for git hub")
-print("my name is nazish")
+str = "nazish noor"
+print(len(str))
